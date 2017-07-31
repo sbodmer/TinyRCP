8224622da562e73ccc4334b898a1f3d6ad9ea2e9
Stephan Bodmer <sbodmer@lsi-media.ch>
2017-07-31 17:06:05 +0200

    Added MD5 static method in main App

--- src/org/tinyrcp/App.java
+++ src/org/tinyrcp/App.java
@@ -475,4 +475,26 @@ public class App {
 
     }
     
+    /**
+     * Find the md5 for a string
+     * 
+     * @param md5
+     * @return 
+     */
+    static public String MD5(String md5) {
+        if (md5 == null) return "";
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+
+        } catch (java.security.NoSuchAlgorithmException e) {
+
+        }
+        return null;
+    }
 }
